925b8451dca6c016e39f8ac224814d4bb15ebd43
doublep2022 <99788967+doublep2022@users.noreply.github.com>
2023-12-25 15:43:30 +0700

    Add files via upload

---
+++ POPAPIService/ProjectDeliveryAPI/Controllers/IssueController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectDeliveryAPI.Models;
+namespace ProjectDeliveryAPI.Controllers
+{
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class IssueController : ControllerBase
+        {
+            // GET: api/<StudentAdmissionController>
+            [HttpGet]
+            public IEnumerable<IssueModel> Get()
+            {
+                IssueModel issueModel1 = new IssueModel();
+                IssueModel issueModel2 = new IssueModel();
+                issueModel1.IssueId = 1;
+                issueModel1.Subject = "Joins Release 2.1 Maybe Delay";
+                issueModel1.IssueBy = "DuongJai";
+                issueModel1.Status = "Pending";
+                issueModel1.IssueDate = DateTime.Now;
+                issueModel2.IssueId = 2;
+                issueModel2.Subject = "POP phase 2 Additional Requirement";
+                issueModel2.IssueBy = "Napa";
+                issueModel2.Status = "Closed";
+                issueModel2.IssueDate = DateTime.Now;
+                List<IssueModel> lissuemodel = new List<IssueModel>
+            {
+                issueModel1,
+                issueModel2
+            };
+                return lissuemodel;
+            }
+        }
+    
+}
---
+++ POPAPIService/ProjectDeliveryAPI/Models/IssueModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectDeliveryAPI.Models
+{
+    public class IssueModel
+    {
+        public int IssueId { get; set; }
+        public string Subject { get; set; }
+        public string IssueBy { get; set; }
+        public string Status { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+}
