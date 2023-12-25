5786e5a1925d88c6e3b6fa77f43fe755b4ab6033
doublep2022 <99788967+doublep2022@users.noreply.github.com>
2023-12-25 15:44:56 +0700

    Add files via upload

---
+++ POPAPIService/SaleFunnelAPI/Controllers/LeadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SaleFunnelAPI.Models;
+namespace SaleFunnelAPI.Controllers
+{
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class LeadController : ControllerBase
+        {
+            // GET: api/<LeadController>
+            [HttpGet]
+            public IEnumerable<LeadModel> Get()
+            {
+                LeadModel leadModel1 = new LeadModel();
+                LeadModel leadModel2 = new LeadModel();
+                leadModel1.LeadId = 1;
+                leadModel1.LeadProjectName = "Paperless System";
+                leadModel1.Customer = "AWC";
+                leadModel1.Status = "Award";
+                leadModel1.IssueDate = DateTime.Now;
+                leadModel2.LeadId = 2;
+                leadModel2.LeadProjectName = "PICO Loan Refinance System";
+                leadModel2.Customer = "ลิ้มสุขใจ";
+                leadModel2.Status = "Follow up";
+                leadModel2.IssueDate = DateTime.Now;
+                List<LeadModel> lLeadModel = new List<LeadModel>
+            {
+                leadModel1,
+                leadModel2
+            };
+                return lLeadModel;
+            }
+        }
+    
+}
---
+++ POPAPIService/SaleFunnelAPI/Models/LeadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SaleFunnelAPI.Models
+{
+    public class LeadModel
+    {
+        public int LeadId { get; set; }
+        public string? LeadProjectName { get; set; }
+        public string? Customer { get; set; }
+        public string? Status { get; set; }
+        public DateTime IssueDate { get; set; }
+    }
+}
